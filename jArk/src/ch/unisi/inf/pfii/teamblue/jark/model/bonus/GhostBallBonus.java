4a5947fdba57799b7e702279366ad8bb3beecf22
selbert@lu.unisi.ch <selbert@lu.unisi.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-04-20 18:25:35 +0000

    added javadoc headers to bonus

--- jArk/src/ch/unisi/inf/pfii/teamblue/jark/model/bonus/GhostBallBonus.java
+++ jArk/src/ch/unisi/inf/pfii/teamblue/jark/model/bonus/GhostBallBonus.java
@@ -2,6 +2,13 @@ package ch.unisi.inf.pfii.teamblue.jark.model.bonus;
 
 import ch.unisi.inf.pfii.teamblue.jark.model.Bonus;
 
+/**
+ * 
+ * @author Stefano.Pongelli@lu.unisi.ch, Thomas.Selber@lu.unisi.ch
+ * @version $LastChangedDate$
+ *
+ */
+
 public class GhostBallBonus extends Bonus {
 
 }

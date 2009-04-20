cd7dfe84e76e831127f8204a2773b227456337c4
j0k3r@k4s.ch <j0k3r@k4s.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-04-20 12:26:46 +0000

    corrected cacca string

--- jArk/src/ch/unisi/inf/pfii/blue/Game.java
+++ jArk/src/ch/unisi/inf/pfii/blue/Game.java
@@ -22,7 +22,7 @@ public class Game {
 		bonuses = new ArrayList<Bonus>();
 		balls.add(new Ball(0, 0));
 		vaus = new Vaus(0, 0, 0);
-		player = new Player("Cacca", 3);
+		player = new Player("pippo", 3);
 		level = new Level(25);
 		
 	}

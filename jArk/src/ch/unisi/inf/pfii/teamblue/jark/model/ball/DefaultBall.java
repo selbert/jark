ad8e3af57bfef7016fe40e91f8a4c17d390f55d7
selbert@lu.unisi.ch <selbert@lu.unisi.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-04-21 13:21:05 +0000

    added defaultball

---
+++ jArk/src/ch/unisi/inf/pfii/teamblue/jark/model/ball/DefaultBall.java
@@ -0,0 +1,12 @@
+package ch.unisi.inf.pfii.teamblue.jark.model.ball;
+
+import ch.unisi.inf.pfii.teamblue.jark.model.Ball;
+
+public class DefaultBall extends Ball {
+
+	public DefaultBall(final int x, final int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+}

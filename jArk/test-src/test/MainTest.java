1eca956e71627bef19bce2699eea1e692873c88f
selbert@lu.unisi.ch <selbert@lu.unisi.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-04-23 19:46:55 +0000

    added ball test and correcter print() method in ball, modified to toString()

--- jArk/test-src/test/MainTest.java
+++ jArk/test-src/test/MainTest.java
@@ -1,11 +1,30 @@
 package test;
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
-import ch.unisi.inf.pfii.teamblue.jark.*;
+import ch.unisi.inf.pfii.teamblue.jark.model.Game;
+import ch.unisi.inf.pfii.teamblue.jark.model.ball.Ball;
+import ch.unisi.inf.pfii.teamblue.jark.model.ball.DefaultBall;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.Bonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.level.Level;
+import ch.unisi.inf.pfii.teamblue.jark.model.vaus.Vaus;
 
 
 public class MainTest extends TestCase {
 	
 	public void testLevelCreationFromFile() {
 		assertTrue(true);
+	}
+	public void testBallMove() {
+		ArrayList<Bonus> freeBonuses = new ArrayList<Bonus>();
+		Vaus vaus = new Vaus(Game.getGAME_WIDTH() - 20, 20);
+		Level level = new Level(25, freeBonuses);
+		Ball ball = new DefaultBall(vaus, level);
+		ball.setSpeedX(4);
+		ball.move();
+		ball.move();
+		assertTrue(ball.toString().equals("798 552 4 0"));
+		ball.move();
+		assertTrue(ball.toString().equals("799 552 -4 0"));
 	}
 }

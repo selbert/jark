277d652392e97ba4e94c785f958df7cda96f0fa8
selbert@lu.unisi.ch <selbert@lu.unisi.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-05-01 11:43:42 +0000

    fixed vaus deviation speed

--- trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/model/ball/Ball.java
+++ trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/model/ball/Ball.java
@@ -126,7 +126,7 @@ public abstract class Ball implements Constants {
 		
 		if (newY + (BALL_RADIUS*2) > VAUS_Y-1 && newY + (BALL_RADIUS*2) < VAUS_Y+5 && newX + (BALL_RADIUS*2) >= vaus.getX() && newX <= vaus.getX() + VAUS_WIDTH) {
 			speedY = -speedY;
-			speedX = (newX - (vaus.getX() + (VAUS_WIDTH / 2))) / 20;
+			speedX = ((newX + BALL_RADIUS) - (vaus.getX() + (VAUS_WIDTH / 2))) / 10;
 			newY =  VAUS_Y-1 - (BALL_RADIUS*2);
 		}
 		x = newX;

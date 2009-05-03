bd2ef9d8e3a9112b94204c43116b3d006a524044
selbert@lu.unisi.ch <selbert@lu.unisi.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-05-03 18:42:16 +0000

    ball refactoring

--- trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/model/ball/Ball.java
+++ trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/model/ball/Ball.java
@@ -56,60 +56,15 @@ public abstract class Ball implements Constants {
 		}
 		
 		if (newY < FIELD_HEIGHT) { 
-			if (level.brickHasBallInside(x, newY)) {
-				speedY = -speedY;
-				if (level.brickHasBallInside(x + (2*BALL_RADIUS), newY)) {
-					level.removeBrick(x + BALL_RADIUS, newY);
-				} else {
-					level.removeBrick(x, newY);
-				}
-				newY = y;
-			} else if (level.brickHasBallInside(x + (2*BALL_RADIUS), newY)) {
-				speedY = -speedY;
-				level.removeBrick(x + (2*BALL_RADIUS), newY);
-				newY = y;
-			} else if (level.brickHasBallInside(x, newY + (2*BALL_RADIUS))) {
-				speedY = -speedY;
-				if (level.brickHasBallInside(x + (2*BALL_RADIUS), newY + (2*BALL_RADIUS))) {
-					level.removeBrick(x + BALL_RADIUS, newY + (2*BALL_RADIUS));
-				} else {
-					level.removeBrick(x, newY + (2*BALL_RADIUS));
-				}
-				newY = y;
-			} else if (level.brickHasBallInside(x + (2*BALL_RADIUS), newY + (2*BALL_RADIUS))) {
-				speedY = -speedY;
-				level.removeBrick(x + (2*BALL_RADIUS), newY + (2*BALL_RADIUS));
+			if (bounceY(newY)){
 				newY = y;
 			}
-
 			
-			if (level.brickHasBallInside(newX, y)) {
-				speedX = -speedX;
-				if(level.brickHasBallInside(newX, y + (2*BALL_RADIUS))) {
-					level.removeBrick(newX, y + BALL_RADIUS);
-				} else {
-					level.removeBrick(newX, y);
-				}
+			if (bounceX(newX)){
 				newX = x;
-			} else if (level.brickHasBallInside(newX, y + (2*BALL_RADIUS))) {
-				speedX = -speedX;
-				level.removeBrick(newX, y + (2*BALL_RADIUS));
-				newX = x;
-			} else if (level.brickHasBallInside(newX + (2*BALL_RADIUS), y)) {
-				speedX = -speedX;
-				if (level.brickHasBallInside(newX + (2*BALL_RADIUS), y + (2*BALL_RADIUS))) {
-					level.removeBrick(newX + (2*BALL_RADIUS), y + BALL_RADIUS);
-				} else {
-					level.removeBrick(newX + (2*BALL_RADIUS), y);
-				}
-				newX = x;
-			} else if (level.brickHasBallInside(newX + (2*BALL_RADIUS), y + (2*BALL_RADIUS))) {
-				speedX = -speedX;
-				level.removeBrick(newX + (2*BALL_RADIUS), y + (2*BALL_RADIUS));
-				newX = x;
-			} 
-			
+			}
 		}
+		
 		if (newX + (BALL_RADIUS*2) >= GAME_WIDTH) {
 			speedX = -speedX;
 			newX = GAME_WIDTH - (BALL_RADIUS*2);
@@ -124,15 +79,82 @@ public abstract class Ball implements Constants {
 		}
 		
 		
-		if (newY + (BALL_RADIUS*2) > VAUS_Y-1 && newY + (BALL_RADIUS*2) < VAUS_Y+5 && newX + (BALL_RADIUS*2) >= vaus.getX() && newX <= vaus.getX() + VAUS_WIDTH) {
-			speedY = -speedY;
-			speedX = ((newX + BALL_RADIUS) - (vaus.getX() + (VAUS_WIDTH / 2))) / 10;
+		if (bounceVaus(newX, newY)) {
 			newY =  VAUS_Y-1 - (BALL_RADIUS*2);
 		}
 		x = newX;
 		y = newY;
 	}
+	
+	private boolean bounceVaus(final int newX, final int newY) {
+		if (newY + (BALL_RADIUS*2) > VAUS_Y-1 && newY + (BALL_RADIUS*2) < VAUS_Y+5 && newX + (BALL_RADIUS*2) >= vaus.getX() && newX <= vaus.getX() + VAUS_WIDTH) {
+			speedY = -speedY;
+			speedX = ((newX + BALL_RADIUS) - (vaus.getX() + (VAUS_WIDTH / 2))) / 10;
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean bounceX(final int newX) {
+		if (level.brickHasBallInside(newX, y)) {
+			speedX = -speedX;
+			if(level.brickHasBallInside(newX, y + (2*BALL_RADIUS))) {
+				level.removeBrick(newX, y + BALL_RADIUS);
+			} else {
+				level.removeBrick(newX, y);
+			}
+			return true;
+		} else if (level.brickHasBallInside(newX, y + (2*BALL_RADIUS))) {
+			speedX = -speedX;
+			level.removeBrick(newX, y + (2*BALL_RADIUS));
+			return true;
+		} else if (level.brickHasBallInside(newX + (2*BALL_RADIUS), y)) {
+			speedX = -speedX;
+			if (level.brickHasBallInside(newX + (2*BALL_RADIUS), y + (2*BALL_RADIUS))) {
+				level.removeBrick(newX + (2*BALL_RADIUS), y + BALL_RADIUS);
+			} else {
+				level.removeBrick(newX + (2*BALL_RADIUS), y);
+			}
+			return true;
+		} else if (level.brickHasBallInside(newX + (2*BALL_RADIUS), y + (2*BALL_RADIUS))) {
+			speedX = -speedX;
+			level.removeBrick(newX + (2*BALL_RADIUS), y + (2*BALL_RADIUS));
+			return true;
+		} 
+		return false;
+	}
+	
+	
+	private boolean bounceY(final int newY) {
+		if (level.brickHasBallInside(x, newY)) {
+			speedY = -speedY;
+			if (level.brickHasBallInside(x + (2*BALL_RADIUS), newY)) {
+				level.removeBrick(x + BALL_RADIUS, newY);
+			} else {
+				level.removeBrick(x, newY);
+			}
+			return true;
+		} else if (level.brickHasBallInside(x + (2*BALL_RADIUS), newY)) {
+			speedY = -speedY;
+			level.removeBrick(x + (2*BALL_RADIUS), newY);
+			return true;
+		} else if (level.brickHasBallInside(x, newY + (2*BALL_RADIUS))) {
+			speedY = -speedY;
+			if (level.brickHasBallInside(x + (2*BALL_RADIUS), newY + (2*BALL_RADIUS))) {
+				level.removeBrick(x + BALL_RADIUS, newY + (2*BALL_RADIUS));
+			} else {
+				level.removeBrick(x, newY + (2*BALL_RADIUS));
+			}
+			return true;
+		} else if (level.brickHasBallInside(x + (2*BALL_RADIUS), newY + (2*BALL_RADIUS))) {
+			speedY = -speedY;
+			level.removeBrick(x + (2*BALL_RADIUS), newY + (2*BALL_RADIUS));
+			return true;
+		}
+		return false;
 
+	}
+	
 	public boolean isDead() {
 		return dead;
 	}

b4bf525b40cab1b9fac29295880059df9d2d58fe
selbert@lu.unisi.ch <selbert@lu.unisi.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-05-07 13:46:32 +0000

    added tests

--- trunk/jArk/test-src/test/MainTest.java
+++ trunk/jArk/test-src/test/MainTest.java
@@ -2,13 +2,46 @@ package test;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import sun.tools.native2ascii.Main;
+
 import junit.framework.TestCase;
 import ch.unisi.inf.pfii.teamblue.jark.implementation.Constants;
 import ch.unisi.inf.pfii.teamblue.jark.implementation.Utils;
+import ch.unisi.inf.pfii.teamblue.jark.model.Game;
+import ch.unisi.inf.pfii.teamblue.jark.model.Player;
+import ch.unisi.inf.pfii.teamblue.jark.model.ball.Ball;
+import ch.unisi.inf.pfii.teamblue.jark.model.ball.BoxBall;
 import ch.unisi.inf.pfii.teamblue.jark.model.ball.DefaultBall;
+import ch.unisi.inf.pfii.teamblue.jark.model.ball.ExplosiveBall;
+import ch.unisi.inf.pfii.teamblue.jark.model.ball.FastBall;
+import ch.unisi.inf.pfii.teamblue.jark.model.ball.RubberBall;
+import ch.unisi.inf.pfii.teamblue.jark.model.ball.SlowBall;
+import ch.unisi.inf.pfii.teamblue.jark.model.ball.StickyBall;
+import ch.unisi.inf.pfii.teamblue.jark.model.ball.UltraBall;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.AddLifeBonus;
 import ch.unisi.inf.pfii.teamblue.jark.model.bonus.Bonus;
 import ch.unisi.inf.pfii.teamblue.jark.model.bonus.Bonuses;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.DoubleBallBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.DoubleLaserVausBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.ExplosiveBallBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.FalseBallsBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.FastBallBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.LaserVausBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.LongVausBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.MissileVausBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.RemoveLifeBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.ResetStatusBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.RubberBallBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.ShortVausBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.SlowBallBonus;
 import ch.unisi.inf.pfii.teamblue.jark.model.bonus.StickyBallBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.TheBoxBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.bonus.UltraBallBonus;
+import ch.unisi.inf.pfii.teamblue.jark.model.brick.Brick;
+import ch.unisi.inf.pfii.teamblue.jark.model.brick.DefaultBrick;
+import ch.unisi.inf.pfii.teamblue.jark.model.brick.PersistentBrick;
+import ch.unisi.inf.pfii.teamblue.jark.model.brick.ResistentBrick;
+import ch.unisi.inf.pfii.teamblue.jark.model.brick.VeryResistentBrick;
 import ch.unisi.inf.pfii.teamblue.jark.model.level.Level;
 import ch.unisi.inf.pfii.teamblue.jark.model.vaus.DefaultVaus;
 import ch.unisi.inf.pfii.teamblue.jark.model.vaus.Vaus;
@@ -21,8 +54,121 @@ public class MainTest extends TestCase implements Constants {
 	 * level from a file works(visual/running test)
 	 */
 	public void testLevelCreationFromFile() {
+		Game g = new Game();
+		Player p = new Player("pippo", 2);
+		p.setLives(3);
+		assertTrue(p.getLives() == 3);
+		p.setScore(100);
+		assertTrue(p.getScore() == 100);
+		assertTrue(p.getName().equals("pippo"));
+		p.incrementLives();
+		assertTrue(p.getLives() == 4);
 		assertTrue(true);
+		DefaultBall ball = new DefaultBall(g.getVaus() , g.getBalls().get(0).getLevel() );
+		g.addBall(ball);
+		
 	}
+	
+	public void testBallCreation() {
+		ArrayList<Bonus> freeBonuses = new ArrayList<Bonus>();
+		Vaus vaus = new DefaultVaus(GAME_WIDTH - 20);
+		Level level = new Level(0, freeBonuses, vaus);
+		Ball testBall = new DefaultBall(vaus, level);
+		assertTrue(testBall instanceof Ball);
+		testBall = new BoxBall(vaus, level);
+		testBall.setX(50);
+		testBall.setY(401);
+		testBall.setSpeedY(-5);
+		testBall.move();
+		testBall.setX(50);
+		testBall.setY(30);
+		testBall.setSpeedY(5);
+		testBall.move();
+		assertTrue(testBall instanceof Ball);
+		testBall = new ExplosiveBall(vaus, level);
+		testBall.setX(50);
+		testBall.setY(401);
+		testBall.setSpeedY(-5);
+		testBall.move();
+		assertTrue(testBall instanceof Ball);
+		testBall = new FastBall(vaus, level);
+		testBall.setX(50);
+		testBall.setY(401);
+		testBall.setSpeedY(-5);
+		testBall.move();
+		assertTrue(testBall instanceof Ball);
+		testBall = new RubberBall(vaus, level);
+		testBall.setX(50);
+		testBall.setY(401);
+		testBall.setSpeedY(-5);
+		testBall.move();
+		assertTrue(testBall instanceof Ball);
+		testBall = new SlowBall(vaus, level);
+		testBall.setX(50);
+		testBall.setY(401);
+		testBall.setSpeedY(-5);
+		testBall.move();
+		assertTrue(testBall instanceof Ball);
+		testBall = new StickyBall(vaus, level);
+		assertTrue(testBall instanceof Ball);
+		testBall = new UltraBall(vaus, level);
+		testBall.setX(50);
+		testBall.setY(401);
+		testBall.setSpeedY(-5);
+		testBall.move();
+		assertTrue(testBall instanceof Ball);
+		
+	}
+	
+	public void testBonusCreation() {
+		Bonus bonus = new AddLifeBonus();
+		assertTrue(bonus.toString().equals("bonus_addlife"));
+		bonus = new DoubleBallBonus();
+		assertTrue(bonus.toString().equals("bonus_doubleball"));
+		bonus = new ExplosiveBallBonus();
+		assertTrue(bonus.toString().equals("bonus_explosionball"));
+		bonus = new FalseBallsBonus();
+		assertTrue(bonus.toString().equals("malus_falseball"));
+		bonus = new FastBallBonus();
+		assertTrue(bonus.toString().equals("neutral_fastball"));
+		bonus = new LaserVausBonus();
+		assertTrue(bonus.toString().equals("bonus_laservaus"));
+		bonus = new LongVausBonus();
+		assertTrue(bonus.toString().equals("bonus_longvaus"));
+		bonus = new MissileVausBonus();
+		assertTrue(bonus.toString().equals("bonus_missilevaus"));
+		bonus = new ResetStatusBonus();
+		assertTrue(bonus.toString().equals("neutral_resetstatus"));
+		bonus = new RubberBallBonus();
+		assertTrue(bonus.toString().equals("malus_rubberball"));
+		bonus = new RemoveLifeBonus();
+		assertTrue(bonus.toString().equals("malus_removelife"));
+		bonus = new ShortVausBonus();
+		assertTrue(bonus.toString().equals("malus_shortvaus"));
+		bonus = new SlowBallBonus();
+		assertTrue(bonus.toString().equals("neutral_slowball"));
+		bonus = new StickyBallBonus();
+		assertTrue(bonus.toString().equals("neutral_stickyball"));
+		bonus = new TheBoxBonus();
+		assertTrue(bonus.toString().equals("bonus_box"));
+		bonus = new UltraBallBonus();
+		assertTrue(bonus.toString().equals("bonus_ultraball"));
+		bonus = new DoubleLaserVausBonus();
+		assertTrue(bonus.toString().equals("bonus_doublelaservaus"));
+		
+	}
+	
+	public void testBrickCreation() {
+		Brick brick = new DefaultBrick();
+		assertTrue(brick.toString().equals("defaultBrick"));
+		brick = new PersistentBrick();
+		assertTrue(brick.toString().equals("persistentBrick"));
+		brick = new ResistentBrick();
+		assertTrue(brick.toString().equals("resistentBrick2"));
+		brick = new VeryResistentBrick();
+		assertTrue(brick.toString().equals("veryResistentBrick3"));
+	}
+
 	public void testBallMove() {
 		ArrayList<Bonus> freeBonuses = new ArrayList<Bonus>();
 		Vaus vaus = new DefaultVaus(GAME_WIDTH - 20);
@@ -71,6 +217,10 @@ public class MainTest extends TestCase implements Constants {
 	}
 	
 	public void testBrickHasBallInside() {
-		//TODO
+		ArrayList<Bonus> freeBonuses = new ArrayList<Bonus>();
+		Vaus vaus = new DefaultVaus(GAME_WIDTH - 20);
+		Level level = new Level(0, freeBonuses, vaus);
+		assertFalse(level.brickHasBallInside(0f, 401f));
+		assertTrue(level.brickHasBallInside(0f, 398f));
 	}
 }

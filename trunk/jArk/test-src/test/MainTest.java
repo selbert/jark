c2d23456ceefd6ead6fb86c6162e98a143c787a4
selbert@lu.unisi.ch <selbert@lu.unisi.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-05-07 13:53:24 +0000

    fixed tests

--- trunk/jArk/test-src/test/MainTest.java
+++ trunk/jArk/test-src/test/MainTest.java
@@ -177,17 +177,17 @@ public class MainTest extends TestCase implements Constants {
 		ball.setSpeedX(4);
 		ball.move();
 		ball.move();
-		assertTrue(ball.toString().equals("778.2 543.0 -3.8 -0.0"));
+		assertTrue(ball.toStringTest().equals("778.2 543.0 -3.8 -0.0"));
 		ball.move();
-		assertTrue(ball.toString().equals("774.4 543.0 -3.8 -0.0"));
+		assertTrue(ball.toStringTest().equals("774.4 543.0 -3.8 -0.0"));
 		vaus = new DefaultVaus(0);
 		ball = new DefaultBall(vaus, level);
 		ball.setSpeedX(-4);
 		ball.move();
 		ball.move();
-		assertTrue(ball.toString().equals("37.6 543.0 -0.4 -0.0"));
+		assertTrue(ball.toStringTest().equals("37.6 543.0 -0.4 -0.0"));
 		ball.move();
-		assertTrue(ball.toString().equals("37.199997 543.0 -0.4 -0.0"));
+		assertTrue(ball.toStringTest().equals("37.199997 543.0 -0.4 -0.0"));
 		vaus = new DefaultVaus(0);
 		ball = new DefaultBall(vaus, level);
 		ball.setSpeedY(-5);
@@ -195,9 +195,9 @@ public class MainTest extends TestCase implements Constants {
 			ball.move();
 		}
 		ball.move();
-		assertTrue(ball.toString().equals("42.0 404.0 0.0 -5.0"));
+		assertTrue(ball.toStringTest().equals("42.0 404.0 0.0 -5.0"));
 		ball.move();
-		assertTrue(ball.toString().equals("42.0 404.0 0.0 5.0"));
+		assertTrue(ball.toStringTest().equals("42.0 404.0 0.0 5.0"));
 		//the block in 10,395 disappears.
 	}
 	

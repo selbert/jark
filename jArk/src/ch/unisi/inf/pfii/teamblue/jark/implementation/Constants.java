a24ccbf849c0a5ae7cf484d99f8e636754b0f474
selbert@lu.unisi.ch <selbert@lu.unisi.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-05-19 09:39:59 +0000

    small fixes

--- jArk/src/ch/unisi/inf/pfii/teamblue/jark/implementation/Constants.java
+++ jArk/src/ch/unisi/inf/pfii/teamblue/jark/implementation/Constants.java
@@ -13,13 +13,14 @@ public interface Constants {
 	int GAME_WIDTH = 798;
 	int GAME_HEIGHT = 600;
 	int BALL_RADIUS = 8;
+	float BALL_SPEED = 3;
 	int BONUS_SPEED = 2;
 	int VAUS_SPEED = 8;
-	int VAUS_Y = 560;
+	int VAUS_HEIGHT = 20;
+	int VAUS_Y = GAME_HEIGHT - 40;
 	int VAUS_WIDTH = 100;
 	int LONGVAUS_WIDTH = 150;
 	int SHORTVAUS_WIDTH = 65;
-	int VAUS_HEIGHT = 20;
 	int FIELD_WIDTH = 798;
 	int FIELD_HEIGHT = 400;
 	int FIELD_COLUMNS = 14;

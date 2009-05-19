24bcc340265c21c27db720a82be5c0168ac20836
j0k3r@k4s.ch <j0k3r@k4s.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-05-19 22:58:59 +0000

    refactoring/fixing/adding features to level editor

--- trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/view/levelcreator/BricksPanel.java
+++ trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/view/levelcreator/BricksPanel.java
@@ -1,11 +1,17 @@
 package ch.unisi.inf.pfii.teamblue.jark.view.levelcreator;
 
+import java.awt.Container;
+import java.awt.Cursor;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -24,31 +30,57 @@ import ch.unisi.inf.pfii.teamblue.jark.view.ImagesRepository;
 public final class BricksPanel extends JPanel {
 	
 	public BricksPanel(ButtonGroup group) {
-		setLayout(new GridLayout(1,0));
-		
-		JLabel bricks = new JLabel("Bricks", SwingConstants.CENTER);
-		add(bricks);
+		setLayout(new GridBagLayout());
+		final GridBagConstraints gbc = new GridBagConstraints();
 		
 		final String[] str = new String[] { "defaultBrick", "resistentBrick", "veryResistentBrick", "persistentBrick", "removeBrick"  };
-	
-		for (int i = 0; i<str.length; i++) {
-			final ImageIcon im = ImagesRepository.getIcon(str[i]);
-			final ImageIcon him = ImagesRepository.getHighlightedIcon(im);
-			 JRadioButton button = new JRadioButton(im);
-			button.setSelectedIcon(him);
-			button.setHorizontalAlignment(SwingConstants.CENTER);
-			button.setActionCommand(str[i]);
-			button.setToolTipText(str[i]);
-			
-			group.add(button);
-			add(button);
-		}
-		
-		
 
-	
+		gbc.ipadx = 20;
+		gbc.ipady = 15;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		add(createButtonPanel("defaultBrick", group), gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		add(createButtonPanel("resistentBrick", group), gbc);
 		
-	
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		add(createButtonPanel("veryResistentBrick", group), gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		add(createButtonPanel("persistentBrick", group), gbc);
+		
+		gbc.gridx = 2;
+		gbc.gridy = 0;
+		gbc.gridheight = 2;
+		add(createButtonPanel("removeBrick", group), gbc);
+		
 	}
 	
+	private final JPanel createButtonPanel(String buttonString, ButtonGroup group) {
+		final ImageIcon im = ImagesRepository.getIcon(buttonString);
+		final ImageIcon him = ImagesRepository.getHighlightedIcon(im);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridBagLayout());
+		final GridBagConstraints gbc = new GridBagConstraints();
+		
+		gbc.gridy = 0;
+		JRadioButton button = new JRadioButton(im);
+		button.setSelectedIcon(him);
+		button.setActionCommand(buttonString);
+		button.setToolTipText(buttonString);
+		button.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		group.add(button);
+		panel.add(button,gbc);
+		
+		gbc.gridy = 1;
+		JLabel desc = new JLabel(buttonString);
+		panel.add(desc,gbc);
+		
+		return panel;
+	}
 }
--- trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/view/levelcreator/OptionPanel.java
+++ trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/view/levelcreator/OptionPanel.java
@@ -1,7 +1,9 @@
 package ch.unisi.inf.pfii.teamblue.jark.view.levelcreator;
 
+import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
@@ -19,106 +21,185 @@ import javax.swing.text.PlainDocument;
 
 @SuppressWarnings("serial")
 public class OptionPanel extends JPanel {
+	private JTextField levelAuthor;
+	private JTextField levelName;
+	private JCheckBox randomBonus;
+	private JTextField numOfBonuses;
 	
 	@SuppressWarnings("serial")
 	public OptionPanel(final JTabbedPane tabbedPane, final FieldPanel fieldPanel) {
 		setLayout(new GridBagLayout());
-		final GridBagConstraints gbc = new GridBagConstraints();
 		
+		class AcceptedDocument extends PlainDocument {
+			private final JTextField parent;
+			private final boolean numerical;
+			
+			public AcceptedDocument(final JTextField parent, final boolean numerical) {
+				this.parent = parent;
+				this.numerical = numerical;
+			}
+			@Override
+			public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+				super.insertString(offs, str, a);
+				checkAccepted();	
+			}
+			@Override
+			protected void postRemoveUpdate(DefaultDocumentEvent chng) {
+				super.postRemoveUpdate(chng);
+				checkAccepted();
+			}
+			private void checkAccepted() {
+				boolean accepted;
+				if (numerical) {
+					accepted = (!randomBonus.isSelected() || parent.getText().matches("[0-9]{1,2}"));
+				} else {
+					accepted = (parent.getText().matches("[a-zA-Z_][a-zA-Z_0-9]*") && parent.getText().length() < 13);
+				}
+				if (accepted) {
+					parent.setBackground(new JTextField().getBackground());
+				} else {
+					parent.setBackground(Color.RED);
+				}	
+			}
+			
+		};
+		
+		final GridBagConstraints gbcOuter = new GridBagConstraints();
+		final GridBagConstraints gbcInner = new GridBagConstraints();
+		
+		//------- level info -------
+		gbcOuter.gridx = 0;
+		gbcOuter.gridy = 0;
+		gbcOuter.fill = GridBagConstraints.BOTH;
+		gbcOuter.ipadx = 50;
+		gbcOuter.ipady = 20;
+		gbcOuter.insets = new Insets(0,0,0,10);
 		JPanel levelInfo = new JPanel();
 		levelInfo.setLayout(new GridBagLayout());
 		levelInfo.setBorder(BorderFactory.createTitledBorder("Level Info"));
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.anchor = GridBagConstraints.WEST;
+		
+		gbcInner.gridx = 0;
+		gbcInner.gridy = 0;
+		gbcInner.insets = new Insets(2,2,2,2);
+		gbcInner.anchor = GridBagConstraints.WEST;
 		JLabel levelNameLabel = new JLabel("Level Name: ");
-		levelInfo.add(levelNameLabel, gbc);
-		gbc.gridx = 1;
-		gbc.gridy = 0;
-		JTextField levelName = new JTextField(10);
-		levelName.setDocument(new PlainDocument() {
-			@Override
-			public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
-				if (str != null && str.matches("\\w") && offs < 12) {
-					super.insertString(offs, str, a);
-				} else {
-					Toolkit.getDefaultToolkit().beep();
-				}	
-			}
-		});
-		levelName.setHorizontalAlignment(JTextField.CENTER);
+		levelInfo.add(levelNameLabel, gbcInner);
+		
+		gbcInner.gridx = 1;
+		gbcInner.gridy = 0;
+		levelName = new JTextField(10);
 		levelName.setBorder(new EtchedBorder());
-		levelInfo.add(levelName, gbc);
-		gbc.gridx = 0;
-		gbc.gridy = 1;
+		levelName.setDocument(new AcceptedDocument(levelName, false));
+		levelName.setHorizontalAlignment(JTextField.CENTER);
+		levelInfo.add(levelName, gbcInner);
+		
+		gbcInner.gridx = 0;
+		gbcInner.gridy = 1;
 		JLabel levelAuthLabel = new JLabel("Level Author: ");
-		levelInfo.add(levelAuthLabel, gbc);
-		gbc.gridx = 1;
-		gbc.gridy = 1;
-		JTextField levelAuthor = new JTextField(10);
-		levelAuthor.setDocument(new PlainDocument() {
-			@Override
-			public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
-				if (str != null && str.matches("\\w") && offs < 12) {
-					super.insertString(offs, str, a);
-				} else {
-					Toolkit.getDefaultToolkit().beep();
-				}	
-			}
-		});
+		levelInfo.add(levelAuthLabel, gbcInner);
+		
+		gbcInner.gridx = 1;
+		gbcInner.gridy = 1;
+		levelAuthor = new JTextField(10);
+		levelAuthor.setDocument(new AcceptedDocument(levelAuthor, false));
 		levelAuthor.setHorizontalAlignment(JTextField.CENTER);
 		levelAuthor.setBorder(new EtchedBorder());
-		levelInfo.add(levelAuthor, gbc);
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		add(levelInfo, gbc);
+		levelInfo.add(levelAuthor, gbcInner);
 		
+		add(levelInfo, gbcOuter);
+		
+		//------- others -------
+		gbcOuter.gridx = 1;
+		gbcOuter.gridy = 0;
+		gbcOuter.insets = new Insets(0,10,0,0);
 		JPanel checks = new JPanel();
 		checks.setBorder(BorderFactory.createTitledBorder("Others"));
 		checks.setLayout(new GridBagLayout());
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		JCheckBox overrideCheck = new JCheckBox("Enable bricks override", true);
-		checks.add(overrideCheck, gbc);
 		
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		JCheckBox randomBonus = new JCheckBox("Auto insert % bonuses randomly:", false);
+		gbcInner.gridx = 0;
+		gbcInner.gridy = 0;
+		JCheckBox overrideCheck = new JCheckBox("Enable bricks override", true);
+		overrideCheck.setToolTipText("<html>When enabled bricks in the editor will<br>be replaced with the selected one</html>");
+		overrideCheck.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.DESELECTED) {
+					fieldPanel.setBricksOverride(false);
+				} else {
+					fieldPanel.setBricksOverride(true);
+				}
+			}	
+		});
+		checks.add(overrideCheck, gbcInner);
+		
+		gbcInner.gridx = 0;
+		gbcInner.gridy = 1;
+		randomBonus = new JCheckBox("Auto insert % bonuses randomly:", false);
+		randomBonus.setToolTipText("<html>Enable random bonus assignment at level start</html>");
 		randomBonus.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
 				if (e.getStateChange() == ItemEvent.DESELECTED) {
 					tabbedPane.setEnabledAt(1, true);
 					fieldPanel.setBonusDisplay(true);
+					numOfBonuses.setEnabled(false);
 				} else {
 					tabbedPane.setEnabledAt(1, false);
 					fieldPanel.setBonusDisplay(false);
+					numOfBonuses.setEnabled(true);
 				}
 			}	
 		});
-		checks.add(randomBonus, gbc);
-		gbc.gridx = 1;
-		gbc.gridy = 1;
-		gbc.anchor = GridBagConstraints.CENTER;
-		final JTextField numOfBonuses = new JTextField(3);
+		checks.add(randomBonus, gbcInner);
 		
-		numOfBonuses.setDocument(new PlainDocument() {
-			@Override
-			public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
-				if (str != null && str.matches("\\d") && offs < 2) {
-					super.insertString(offs, str, a);
-				} else {
-					Toolkit.getDefaultToolkit().beep();
-				}	
-			}
-		});
+		gbcInner.gridx = 1;
+		gbcInner.gridy = 1;
+		gbcInner.anchor = GridBagConstraints.CENTER;
+		numOfBonuses = new JTextField(3);
+		numOfBonuses.setToolTipText("<html>Percentage of bonuses (bonus, malus and neutral) over the total number of bricks <br>that will be randomly assigned each time the level starts</html>");
+		numOfBonuses.setDocument(new AcceptedDocument(numOfBonuses, true));
+		numOfBonuses.setText("30");
+		numOfBonuses.setEnabled(false);
 		numOfBonuses.setHorizontalAlignment(JTextField.CENTER);
-		
 		numOfBonuses.setBorder(new EtchedBorder());
-		checks.add(numOfBonuses, gbc);
+		checks.add(numOfBonuses, gbcInner);
 		
-		gbc.anchor = GridBagConstraints.WEST;
-		gbc.gridx = 1;
-		gbc.gridy = 0;
-		add(checks, gbc);
+		add(checks, gbcOuter);
+	}
+	
+	public void setLevelName(String name) {
+		levelName.setText(name);
+	}
+	public String getLevelName() {
+		final String temp = levelName.getText();
+		if (temp == null || !temp.matches("[a-zA-Z_][a-zA-Z_0-9]*")) {
+			return "Anon";
+		} else {
+			return temp;
+		}
+	}
+	
+	public void setLevelAuthor(String author) {
+		levelAuthor.setText(author);
+	}
+	public String getLevelAuthor() {
+		final String temp = levelAuthor.getText();
+		if (temp == null || !temp.matches("[a-zA-Z_][a-zA-Z_0-9]*")) {
+			return "Anonymous";
+		} else {
+			return temp;
+		}
+	}
+	
+	public void setRandomBonusNum(int bonusPercentage) {
+		if (bonusPercentage != 0) {
+			randomBonus.setSelected(true);
+			numOfBonuses.setText(bonusPercentage+"");
+		}
+	}
+	public int getRandomBonusNum() {
+		if (randomBonus.isSelected() && numOfBonuses.getText().matches("[0-9]{1,2}")) {
+			return Integer.parseInt(numOfBonuses.getText());
+		} else {
+			return 0;
+		}
 	}
 }

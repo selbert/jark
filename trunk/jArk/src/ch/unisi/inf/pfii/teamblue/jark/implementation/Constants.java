e91b192c11c8989df8ff242cc8c455a74b92447e
j0k3r@k4s.ch <j0k3r@k4s.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-05-21 18:35:09 +0000

    beta version

--- trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/implementation/Constants.java
+++ trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/implementation/Constants.java
@@ -27,7 +27,7 @@ public interface Constants {
 	int FIELD_ROWS = 16;
 	int BRICK_WIDTH = 57;
 	int BRICK_HEIGHT = 25;
-	int MAX_LEVEL = 1;
+	int MAX_LEVEL = 7;
 	int BALL_LIFE = 100;
 	// bonus lives (ms)
 	int INSTANTANEOUS = 0;
@@ -36,14 +36,14 @@ public interface Constants {
 	int EXPLOSION_BALL = 20000;
 	int FALSE_BALLS = 15000;
 	int FAST_BALL = 20000;
-	int GHOST_BALL = 20000;
+	int GHOST_BALL = 15000;
 	int RIFLE_VAUS = 15000;
 	int CANNON_VAUS = 20000;
 	int SLOW_BALL = 20000;
 	int STICKY_BALL = 20000;
 	int THE_BOX = 20000;
 	int ULTRA_BALL = 10000;
-	int LIGHT_OFF = 10000;
+	int LIGHT_OFF = 20000;
 	// vaus shooting speed delay (ms)
 	int DOUBLE_RIFLE_DELAY = 100;
 	int RIFLE_DELAY = 150;

a81e65170dbdb77929f448129007c894c03fc70e
selbert@lu.unisi.ch <selbert@lu.unisi.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-05-24 12:54:35 +0000

    added comments

--- jArk/src/ch/unisi/inf/pfii/teamblue/jark/implementation/GameListener.java
+++ jArk/src/ch/unisi/inf/pfii/teamblue/jark/implementation/GameListener.java
@@ -6,7 +6,7 @@ import ch.unisi.inf.pfii.teamblue.jark.model.level.Level;
  * Listen to game
  * 
  * @author Stefano.Pongelli@lu.unisi.ch, Thomas.Selber@lu.unisi.ch
- * @version $LastChangedDate: 2009-05-10 01:19:22 +0200 (Sun, 10 May 2009) $
+ * @version $LastChangedDate$
  * 
  */
 
--- jArk/src/ch/unisi/inf/pfii/teamblue/jark/implementation/VausSetListener.java
+++ jArk/src/ch/unisi/inf/pfii/teamblue/jark/implementation/VausSetListener.java
@@ -6,7 +6,7 @@ import ch.unisi.inf.pfii.teamblue.jark.model.vaus.Vaus;
  * Listen to vaus changes
  * 
  * @author Stefano.Pongelli@lu.unisi.ch, Thomas.Selber@lu.unisi.ch
- * @version $LastChangedDate: 2009-05-07 15:47:49 +0200 (Thu, 07 May 2009) $
+ * @version $LastChangedDate$
  * 
  */
 

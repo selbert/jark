9b80a811ff8acd1d7429c474af5e9ea8abd7c90d
selbert@lu.unisi.ch <selbert@lu.unisi.ch@3008a844-2a76-11de-b02e-9db90c89ab99>
2009-05-24 12:54:35 +0000

    added comments

--- trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/implementation/StringEncrypt.java
+++ trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/implementation/StringEncrypt.java
@@ -1,7 +1,25 @@
 package ch.unisi.inf.pfii.teamblue.jark.implementation;
-
+/**
+ * Method to encrypt and decrypt strings using a simple algorithm.
+ * 
+ * @author Stefano.Pongelli@lu.unisi.ch, Thomas.Selber@lu.unisi.ch
+ * @version $LastChangedDate$
+ * 
+ */
 public final class StringEncrypt {
 
+	
+	/**
+	 * Encrypt a string using a code
+	 * 
+	 * @param string
+	 * 				the string you want to encrypt
+	 * @param code
+	 * 				the selected code you want to use to encrypt the string, it will have to be the same to decrypt
+	 * 
+	 * @return encrypted string
+	 */
+	
 	public static final String encrypt(final String string, final int code) {
 		String encryptedString;
 
@@ -16,6 +34,17 @@ public final class StringEncrypt {
 		return encryptedString;
 	}
 
+	/**
+	 * Encrypt a string using a the same code used to encrypt
+	 * 
+	 * @param string
+	 * 				the string you want to decrypt
+	 * @param code
+	 * 				the code you want to use to decrypt the string, it will have to be the same you selected for encrypt
+	 * 
+	 * @return encrypted string
+	 */
+	
 	public static final String decrypt(final String string, final int code) {
 		String dencryptedString;
 
--- trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/implementation/VausListener.java
+++ trunk/jArk/src/ch/unisi/inf/pfii/teamblue/jark/implementation/VausListener.java
@@ -4,7 +4,7 @@ package ch.unisi.inf.pfii.teamblue.jark.implementation;
  * Listen to vaus
  * 
  * @author Stefano.Pongelli@lu.unisi.ch, Thomas.Selber@lu.unisi.ch
- * @version $LastChangedDate: 2009-05-10 01:19:22 +0200 (Sun, 10 May 2009) $
+ * @version $LastChangedDate$
  * 
  */
 
